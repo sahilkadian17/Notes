737ace1341e73abb800051233222338cde61bd6c
Sahil Kadian <sahilkadian17052001@gmail.com>
2020-12-27 19:45:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/notes/NoteDao.kt
@@ -0,0 +1,14 @@
+package com.example.notes
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+    @Delete()
+    suspend fun delete(note: Note)
+    @Query("SELECT * FROM NOTES_TABLE ORDER BY ID ASC")
+    fun getAllNotes():LiveData<List<Note>>
+}
